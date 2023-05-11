b02cde278ce1117671db950c3c87b47f45f34661
Vinay <vinaycs2006@gmail.com>
2023-05-11 11:07:57 -0400

    Added the local files first time
    This is the master branch

---
+++ Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace AWS_API_Demo
+{
+    public class Movie
+    {
+        public string Movie_Name { get; set; }
+        public DateTime Movie_ReleaseDate { get; set; }
+
+        public string Server_IP { get; set; }
+    }
+}
